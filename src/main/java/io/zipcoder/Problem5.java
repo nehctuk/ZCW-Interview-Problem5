35073e81427aff91ab9866dbf1a4a1c0ce6169de
nehctuk <nehctuk@gmail.com>
2017-11-28 10:41:44 -0500

    Problem5

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,44 @@
 package io.zipcoder;
 
 public class Problem5 {
+    public String returnCorrectReversedString(String input) {
+        input = uncapitalizeFirstLetters(input);
+        input = reverseString(input);
+        input = capitalizeFirstLetters(input);
+        return input;
+    }
+
+    public String reverseString(String input){
+        StringBuilder output = new StringBuilder();
+        for (int x = input.split("").length-1; x >= 0 ; x--) {
+            output.append(input.split("")[x]);
+        }
+        return output.toString();
+    }
+
+    public String capitalizeFirstLetters(String input) {
+        StringBuilder output = new StringBuilder();
+        for (int x = 0; x < input.split("").length; x++) {
+            if ((x>0 && input.split("")[x-1].equals(" ")) || x==0) {
+                output.append(input.split("")[x].toUpperCase());
+            }
+            else {
+                output.append(input.split("")[x]);
+            }
+        }
+        return output.toString();
+    }
+
+    public String uncapitalizeFirstLetters(String input) {
+        StringBuilder output = new StringBuilder();
+        for (int x = 0; x < input.split("").length; x++) {
+            if ((x>0 && input.split("")[x-1].equals(" ")) || x==0) {
+                output.append(input.split("")[x].toLowerCase());
+            }
+            else {
+                output.append(input.split("")[x]);
+            }
+        }
+        return output.toString();
+    }
 }
--- src/test/java/io/zipcoder/Problem5Test.java
+++ src/test/java/io/zipcoder/Problem5Test.java
@@ -1,4 +1,39 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem5Test {
+
+    Problem5 problem5 = new Problem5();
+    String input = "I love UNIT Testing";
+
+    @Test
+    public void reverseStringTest() {
+        String expected = "gnitseT TINU evol I";
+        String actual = problem5.reverseString(input);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void uncapitalizeFirstLettersTest() throws Exception {
+        String expected = "i love uNIT testing";
+        String actual = problem5.uncapitalizeFirstLetters(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void capitalizeFirstLettersTest() throws Exception {
+        String expected = "I Love UNIT Testing";
+        String actual = problem5.capitalizeFirstLetters(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void returnCorrectReversedStringTest() {
+        String expected = "Gnitset TINU Evol I";
+        String actual = problem5.returnCorrectReversedString(input);
+        Assert.assertEquals(expected,actual);
+    }
 }
